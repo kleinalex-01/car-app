4a1fde25cabeaab6297a2c1755bc9e964a238295
kleinalex-01 <kleinalex215@gmail.com>
2025-07-03 15:32:42 +0200

    Project set up
    See README.md for more

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project introduction (2025.07.03.)
+
+  This is a React PWA, where you can list and view data on various cars. The main purpose is to practice UI/UX.
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css"
+import 'car-makes-icons/dist/style.css';
+
+export const App: React.FC = () => {
+  return (
+    <div className="app-container">
+      <h1>Welcome to the React App</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  );
+}
