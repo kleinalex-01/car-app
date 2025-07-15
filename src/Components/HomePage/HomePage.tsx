4d7b64a54f2a1d6006eb5029eae4148f51976436
kleinalex-01 <kleinalex215@gmail.com>
2025-07-15 12:24:26 +0200

    Carousel component deleted

--- src/Components/HomePage/HomePage.tsx
+++ src/Components/HomePage/HomePage.tsx
@@ -1,16 +1,10 @@
 import React from "react";
-import Carousel from "./Carousel";
 
 export const HomePage: React.FC = () => {
 
     return (
         <>
-            <Carousel 
-                autoplay={true}
-                pauseOnHover={true}
-                loop={true}
-                round={false}
-            />
+
         </>
     )
 }
